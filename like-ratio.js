5606d7f183b1a0aebbc1b2047456dd0419355980
chapman808 <anthony.n.chapman@gmail.com>
2021-11-12 16:03:19 -0800

    fixed formatting of like ratio

--- like-ratio.js
+++ like-ratio.js
@@ -2,7 +2,5 @@ var views = document.getElementsByClassName("view-count style-scope ytd-video-vi
 views = parseFloat(views.replace(/,/g, ''));
 var likes = document.getElementById("menu-container").getElementsByTagName("yt-formatted-string")[0].ariaLabel
 likes = likes.split(" ")[0].replace(/,/g, '')
-const likeRatio = ((likes/views) * 100).toFixed(5) + '% Like/View Ratio';
-const ratioNode = document.createTextNode(likeRatio);
-const info = document.getElementsByTagName("ytd-video-primary-info-renderer")[0];
-info.appendChild(ratioNode);
+const likeRatio = ((likes/views) * 100).toFixed(2) + '% like/view ratio';
+document.getElementsByClassName('view-count style-scope ytd-video-view-count-renderer')[0].append(" • " + likeRatio);
