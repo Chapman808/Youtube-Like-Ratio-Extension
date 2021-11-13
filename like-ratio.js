1a6dd3a8de78f64768933f6ae6f528b8786a3d04
unknown <anthony.n.chapman@gmail.com>
2021-11-13 11:45:29 -0500

    fixed script management so that refresh is not needed for content script to run

--- like-ratio.js
+++ like-ratio.js
@@ -1,6 +1,13 @@
+//get views, likes
 var views = document.getElementsByClassName("view-count style-scope ytd-video-view-count-renderer")[0].innerHTML.split(" ")[0]
-views = parseFloat(views.replace(/,/g, ''));
 var likes = document.getElementById("menu-container").getElementsByTagName("yt-formatted-string")[0].ariaLabel
+
+//parse views, likes
+views = parseFloat(views.replace(/,/g, ''));
 likes = likes.split(" ")[0].replace(/,/g, '')
-const likeRatio = ((likes/views) * 100).toFixed(2) + '% like/view ratio';
-document.getElementsByClassName('view-count style-scope ytd-video-view-count-renderer')[0].append(" • " + likeRatio);
+
+//calculate like ratio
+var likeRatio = ((likes/views) * 100).toFixed(2) + '% like/view ratio';
+
+var viewText = document.getElementsByClassName('view-count style-scope ytd-video-view-count-renderer')[0].innerText
+document.getElementsByClassName('view-count style-scope ytd-video-view-count-renderer')[0].innerText = viewText.split("•")[0] + " • " + likeRatio;
